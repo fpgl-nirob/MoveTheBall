3385c61a5c8992d240623214f634ae945e2be4dd
hasan53nirob <hasan53nirob@gmail.com>
2020-05-25 00:14:34 +0600

    sss

--- Move The Ball/AppDelegate.swift
+++ Move The Ball/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        // Nirob Move The Ball 1
+        // Nirob Move The Ball 2
         return true
     }
 
